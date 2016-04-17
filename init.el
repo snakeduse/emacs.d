71fdca5952a717cc65a3c6b1fa64d4a2f6a21d37
Aleksey Profatilov <snakeduse@gmail.com>
2016-04-17 21:59:09 +0300

    add sessions

--- init.el
+++ init.el
@@ -18,6 +18,7 @@
   (unless (server-running-p)
     (server-start)))
 
+(require 'init-benchmarking)
 (require 'init-utils)
 (require 'init-elpa)
 
@@ -34,6 +35,7 @@
 (require 'init-themes)
 (require 'init-editing)
 (require 'init-isearch)
+(require 'init-sessions)
 
 (message "Configs loaded")
 (message "Startup time: %g seconds." (float-time (time-since emacs-load-start-time)))
@@ -47,7 +49,8 @@
  '(custom-enabled-themes (quote (sanityinc-solarized-light)))
  '(custom-safe-themes
    (quote
-    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))))
+    ("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(session-use-package t nil (session)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
